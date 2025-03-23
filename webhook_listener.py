1a22fd38bf6f864e0a7a49fd58d1b1a19eb2a80f
Geraint-png <callum.s.stowell@gmail.com>
2025-03-23 10:21:40 +0000

    Add files via upload

--- webhook_listener.py
+++ webhook_listener.py
@@ -62,7 +62,8 @@ def webhook():
 
 
 if __name__ == '__main__':
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
