faec72e1924607daf4be31376ffaf8bedb716d72
Geraint-png <callum.s.stowell@gmail.com>
2025-03-23 18:39:45 +0000

    Add files via upload

--- webhook_listener.py
+++ webhook_listener.py
@@ -7,8 +7,11 @@ import json
 app = Flask(__name__)
 
 # Replace with your actual Telegram bot token and chat ID
-TELEGRAM_BOT_TOKEN = "8090436532:AAEdtagv5svIKQOT0JG1Te6aFsRwjk3rXVA"
-TELEGRAM_CHAT_ID = "8098105128"
+import os
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
